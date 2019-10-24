d0ef9e6c3fcffa04be0837b443b55bebd3bd17da
BertvdV <bert_van_der_veen@hotmail.com>
2019-10-24 10:56:10 +0200

    Update gllvm.TMB.R

--- R/gllvm.TMB.R
+++ R/gllvm.TMB.R
@@ -334,6 +334,7 @@ gllvm.TMB <- function(y, X = NULL, formula = NULL, num.lv = 2, family = "poisson
           }
           idx<-idx+k
         }
+        zetas[,1] <- 0 
         row.names(zetanew) <- colnames(y00); colnames(zetanew) <- paste(min(y):(max(y00)-1),"|",(min(y00)+1):max(y00),sep="")
         zetas<-zetanew
         out$y<-y00
@@ -646,7 +647,7 @@ gllvm.TMB <- function(y, X = NULL, formula = NULL, num.lv = 2, family = "poisson
           }
           idx<-idx+k
         }
-        
+        se.zetanew[,1] <- 0
         out$sd$zeta <- se.zetanew
         row.names(out$sd$zeta) <- colnames(y00); colnames(out$sd$zeta) <- paste(min(y00):(max(y00)-1),"|",(min(y00)+1):max(y00),sep="")
       }
