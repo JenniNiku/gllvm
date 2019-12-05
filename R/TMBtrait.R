17e648b5dba763e7be2d38c4427290c33a4ea48f
Bert van der Veen <bert.vanderveen@nibio.no>
2019-12-05 10:22:25 +0100

    Catch error and replace by message.

--- R/TMBtrait.R
+++ R/TMBtrait.R
@@ -621,7 +621,7 @@ trait.TMB <- function(y, X = NULL,TR=NULL,formula=NULL, num.lv = 2, family = "po
           A.mat <- -sdr[incl,incl] # a x a
           D.mat <- -sdr[incld,incld] # d x d
           B.mat <- -sdr[incl,incld] # a x d
-          cov.mat.mod <- try(MASS::ginv(A.mat-B.mat%*%solve(D.mat)%*%t(B.mat)),silent=T)
+          cov.mat.mod <- invisible(try(MASS::ginv(A.mat-B.mat%*%solve(D.mat)%*%t(B.mat))))
           se <- sqrt(diag(abs(cov.mat.mod)))
           
           incla<-rep(FALSE, length(incl))
