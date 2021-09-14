5145d645f44eef9c0bc08efc44a8170dccd83196
BertvdV <bert_van_der_veen@hotmail.com>
2021-09-14 14:42:03 +0200

    bugfix

--- R/getResidualCov.gllvm.R
+++ R/getResidualCov.gllvm.R
@@ -171,13 +171,13 @@ getResidualCov.gllvm = function(object, adjust = 1, site.index = NULL, ...)
   
   if(object$quadratic==F){
     if(object$num.lv.c>0){
-      if(any(object$params$sigma.lv<0.1)){
+      if(any(ResCov.q[1:object$num.lv.c]<0.1)){
         warning("The residual variance of ",paste(colnames(object$lvs)[which(ResCov.q[1:object$num.lv.c]<0.01)],collapse=", and ")," is very small. This might indicate that the latent variable is nearly perfectly represented by covariates alone. \n")
       }
     }  
   }else{
     if(object$num.lv.c>0){
-      if(any(object$params$sigma.lv<0.01)){
+      if(any((ResCov.q+ResCov.q2)[1:num.lv.c]<0.01)){
         warning("The residual variance related to ",paste(colnames(object$lvs)[which((ResCov.q+ResCov.q2)[1:object$num.lv.c]<0.01)],collapse=", and ")," is very small. This might indicate that the latent variable is nearly perfectly represented by covariates alone. \n")
       }
     }  
