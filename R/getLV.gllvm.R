93ca072ac8708debca006cc6af820dd6478568d2
BertvdV <bert_van_der_veen@hotmail.com>
2021-10-21 12:26:26 +0200

    Decided to change the terminology and remove the "scaled" option. Types are now "residual" (unconstrained without scale), "conditional" (fixed- and random-effects) and "marginal" (only fixed-effects).

--- R/getLV.gllvm.R
+++ R/getLV.gllvm.R
@@ -2,23 +2,20 @@
 #' @description  Extract latent variables from gllvm object.
 #' 
 #' @param object an object of class 'gllvm'.
-#' @param type type of latent variable scores to retrieve from a gllvm object. For models with unconstrained latent variables, defaults to "unconstrained". For models with constrained latent variables, defaults to "constrained". A third option is "scaled", which returns latent variables multiplied with scale parameter of the loadings. Alternatively, "LC" returns linear combination scores without residual error.
+#' @param type type of latent variable scores to retrieve from a gllvm object. For models with unconstrained latent variables, defaults to "residual". For models with constrained latent variables, defaults to conditional. Alternatively, "marginal" returns linear combination scores without residual error.
 #' @param ... not used
 #' 
 #' @details
 #' Function retrieves the site scores for a GLLVM. Each type corresponds to a separate term of the model. For a GLLVM with unconstrained latent variables
-#' the default is "unconstrained". The type "scaled" returns site scores scaled with the diagonal of the species loadings.
+#' the default is "residual".
 #' 
-#' For GLLVMs with constrained latent variables, "constrained" returns the complete site scores, due to both fixed- and latent effects,
+#' For GLLVMs with constrained latent variables, "conditional" returns the complete site scores, due to both fixed- and latent effects,
 #' where the latent effect is always scaled by the diagonal of the species loadings so that it can be small relative to the fixed-effects.
 #' 
-#' Here, "scaled" returns the same as for a GLLVM with unconstrained latent variables, namely the latent effect scaled with the diagonal
-#' of the species loadings. Type "LC" (linear combination scores) returns the site scores only due to fixed-effects.
+#' Type "marginal"  returns linear combination scores, i.e. the site scores only due to fixed-effects.
 #' 
-#' If both unconstrained and constrained latent variables are included in the model, type "LC" returns LC scores for constrained latent variables
-#' but "scaled" scores for unconstrained latent variables.
-#'@seealso  \code{\link{getLV.gllvm}}.
-
+#' If both unconstrained and constrained latent variables are included in the model, type "marginal" returns LC scores for constrained latent variables
+#' but "residual" scores for unconstrained latent variables.
 #'@aliases getLV getLV.gllvm
 #'@method getLV gllvm
 #'@export
@@ -27,26 +24,24 @@
 getLV.gllvm <- function(object, type = NULL, ...)
 {
   if(!is.null(type)){
-    if(!type%in%c("unconstrained","constrained","scaled","LC")){
-    stop("Type should be one of: unconstrained, constrained, scaled or LC")
+    if(!type%in%c("residual","conditional","marginal")){
+    stop("Type should be one of: residual, conditional, or marginal")
     }
-    if(type=="constrained"&object$num.lv.c==0&object$num.RR==0){
+    if(type=="conditional"&object$num.lv.c==0&object$num.RR==0){
       stop("Cannot retrieve constrained latent variables for an unconstrained latent variable model.")
-    }else if(type=="scaled"&object$num.lv.c==0&object$num.lv==0){
-      stop("Cannot retrieve scaled latent variables for a model without latent effect.")
-    }else if(type=="unconstrained"&object$num.lv.c==0&object$num.lv==0){
-      stop("Cannot retrieve unconstrained latent variables for a model without latent effect.")
+    }else if(type=="residual"&object$num.lv.c==0&object$num.lv==0){
+      stop("Cannot retrieve residual latent variables for a model without latent effect.")
     }
   }
   
   if(is.null(type)){
   if(object$num.lv.c==0&object$num.RR==0){
-    type = "unconstrained"
+    type = "residual"
   }else{
     if((object$num.lv.c+object$num.lv)>0){
-      type = "constrained"
+      type = "conditional"
     }else{
-      type = "LC"
+      type = "marginal"
     }
     
   }
@@ -54,14 +49,13 @@ getLV.gllvm <- function(object, type = NULL, ...)
 
   
   n <- nrow(object$y)
-  if(type == "unconstrained"){
+  if(type == "residual"){
     lvs <- object$lvs
-  }else if(type=="scaled"){
-    lvs <- t(t(object$lvs)*object$params$sigma.lv)
-  }else if(type=="constrained"){
-      lvs <- t(t(object$lvs)*object$params$sigma.lv)
+  }else if(type=="conditional"){
+      lvs <- object$lvs
+      lvs[,1:object$num.lv.c] <- t(t(lvs[,1:object$num.lv.c,drop=F])*object$params$sigma.lv[1:object$num.lv.c])
       lvs[,1:object$num.lv.c] <- lvs[,1:object$num.lv.c]+object$lv.X%*%object$params$LvXcoef[,1:object$num.lv.c,drop=F]
-  }else if(type=="LC"&object$num.lv.c>0){
+  }else if(type=="marginal"&object$num.lv.c>0){
     lvs <-object$lv.X%*%object$params$LvXcoef[,1:object$num.lv.c,drop=F]
   }
   
@@ -71,8 +65,8 @@ getLV.gllvm <- function(object, type = NULL, ...)
     lvs <- cbind(object$lv.X%*%object$params$LvXcoef[,-c(1:object$num.lv.c),drop=F], lvs)
   }else if(object$num.RR>0&object$num.lv==0&object$num.lv.c>0){
     lvs <- cbind(lvs[,1:object$num.lv.c,drop=F],object$lv.X%*%object$params$LvXcoef[,-c(1:object$num.lv.c),drop=F])
-  }else if(object$num.lv.c>0&object$num.lv>0&type=="LC"){
-    lvs <- cbind(lvs, t(t(object$lvs[,-c(1:object$num.lv.c)])*object$params$sigma.lv))
+  }else if(object$num.lv.c>0&object$num.lv>0&type=="marginal"){
+    lvs <- cbind(lvs, object$lvs[,-c(1:object$num.lv.c)])
   }
   
   
