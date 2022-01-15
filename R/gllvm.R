270416236f99df93827a782bc4327afcb946ed35
BertvdV <bert_van_der_veen@hotmail.com>
2022-01-15 09:48:40 +0100

    Set randomB to FALSE if num.RR+num.lv.c = 0

--- R/gllvm.R
+++ R/gllvm.R
@@ -386,6 +386,9 @@ gllvm <- function(y = NULL, X = NULL, TR = NULL, data = NULL, formula = NULL, lv
   if(randomB!=FALSE&quadratic!=FALSE&(num.lv.c+num.RR)>0&method=="LA"){
     stop("Constrained model with quadratic responses and random slopes not allowed with method 'LA'")
   }
+  if((num.RR+num.lv.c)==0){
+    randomB <- FALSE
+  }
   
   if(!randomB%in%c(FALSE,"single","P","LV")){
     stop("RandomB should be one of 'single', 'P', or 'LV'")
