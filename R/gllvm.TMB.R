caef18bb4181854bbfd8c9232c716352d3c41bf2
bertv <bert_van_der_veen@hotmail.com>
2022-08-20 12:49:42 +0200

    bugfix

--- R/gllvm.TMB.R
+++ R/gllvm.TMB.R
@@ -197,7 +197,7 @@ gllvm.TMB <- function(y, X = NULL, lv.X = NULL, formula = NULL, family = "poisso
 
   ## Set initial values for model parameters (including dispersion prm) and latent variables
 
-### Seeds
+  ### Seeds
   
   # If number of seeds is less than n.init, sample the seeds randomly, but using the given seed
   if((length(seed) >1) & (length(seed) < n.init)) {
@@ -214,7 +214,7 @@ gllvm.TMB <- function(y, X = NULL, lv.X = NULL, formula = NULL, family = "poisso
 
 
   n.i <- 1
-
+  if(starting.val!="zero"){seed.best <- seed[n.i]}else{seed.best <- NULL}
   out <- list( y = y, X = X, logL = Inf, num.lv = num.lv, num.lv.c = num.lv.c, row.eff = row.eff, family = family, X.design = X, method = method, zeta.struc = zeta.struc)
   #if (n.init > 1)
 
@@ -2102,9 +2102,9 @@ gllvm.TMB <- function(y, X = NULL, lv.X = NULL, formula = NULL, family = "poisso
         }
         
       }
+      seed.best <- seed[n.i]
     }
     
-    seed.best <- seed[n.i]
     n.i <- n.i+1;
   
     }
