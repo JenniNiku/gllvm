4bcbb1dc52a1b7f1dc657391fdade40d552f5057
bertv <bert_van_der_veen@hotmail.com>
2024-05-13 11:58:30 +0200

    label bug

--- R/ordiplot.gllvm.R
+++ R/ordiplot.gllvm.R
@@ -687,7 +687,7 @@ ordiplot.gllvm <- function(object, biplot = FALSE, ind.spp = NULL, alpha = 0.5, 
       
       if(nrow(ends)>0){
       arrows(x0=origin[1],y0=origin[2],x1=ends[,1]+origin[1],y1=ends[,2]+origin[2],col=col[!cats],length=0.1,lty=lty)
-      text(x=origin[1]+ends[,1]*(1+lab.dist),y=origin[2]+ends[,2]*(1+lab.dist),labels = row.names(LVcoef)[idx],col=col[!cats], cex = cex.env)}
+      text(x=origin[1]+ends[,1]*(1+lab.dist),y=origin[2]+ends[,2]*(1+lab.dist),labels = row.names(LVcoef)[idx&!cats],col=col[!cats], cex = cex.env)}
       
       # add points for categorical variables
       if(any(cats)){
