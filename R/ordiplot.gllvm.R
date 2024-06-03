1216d5f91ec142ce1e3a1757269f52d17cb764e9
bertv <bert_van_der_veen@hotmail.com>
2024-06-03 17:39:32 +0200

    correct when MASS::eqscplot is used (when neither of xlim or ylim is used). Current default is annoying when providing either

--- R/ordiplot.gllvm.R
+++ R/ordiplot.gllvm.R
@@ -129,7 +129,7 @@ ordiplot.gllvm <- function(object, biplot = FALSE, ind.spp = NULL, alpha = 0.5, 
   
   if(!is.null(object$lv.X) && is.null(object$lv.X.design))object$lv.X.design <- object$lv.X #for backward compatibility
   # If both scales are not given, use MASS::eqscplot
-  if(("ylim" %in% names(gr_par_list)) & ("xlim" %in% names(gr_par_list))){
+  if(("ylim" %in% names(gr_par_list)) | ("xlim" %in% names(gr_par_list))){
     plotfun <- plot
   } else {
     plotfun <- MASS::eqscplot
