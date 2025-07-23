ef1046a66bfb6014e4e5f14f32ca6026f889af6d
Niku <jemaelni@jyu.fi>
2025-07-23 14:46:32 +0300

    Stop if range of covers not right

--- R/gllvm.R
+++ R/gllvm.R
@@ -977,6 +977,10 @@ gllvm <- function(y = NULL, X = NULL, TR = NULL, data = NULL, formula = NULL, fa
         }
       }
     }
+    
+    if((family %in% c("beta", "betaH", "orderedBeta")) & any(y>1 |y<0))
+      stop("Responses (eg. percentage cover) must be coded in the range between 0 and 1 in case of beta based response models are used, ('beta, 'orderedBeta' or 'betaH'). Please rescale your data.")
+    
     #If not empty but a vector..
     if(!is.null(disp.formula)){
       if(is.vector(disp.formula)){
